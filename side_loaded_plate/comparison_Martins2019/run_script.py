0410a2037bcccd816e4e92980fbc765350f8f73b
bonneted <bonneteymard.damien@gmail.com>
2025-02-24 14:46:37 +0100

    flatten list args and more weight on DIC data

--- side_loaded_plate/comparison_Martins2019/run_script.py
+++ side_loaded_plate/comparison_Martins2019/run_script.py
@@ -6,15 +6,24 @@ executable_path = os.path.join(os.path.dirname(__file__), "../pinn_inverse.py")
 num_runs = 1
 
 args = {
-    "available_time": 5,
+    "available_time": 2,
     "measurments_type": "strain",
     "noise_magnitude": 1e-6,
     "results_path": "comparison_Martins2019/results",
     "log_every": 250,
-    # "log_output_fields": '',
+    "loss_weights": [1,1,1,1,1e2,1e2,1e2],
+    # "log_output_fields": [''],
 }
-args_list = [f"--{key}={value}" for key, value in args.items()] 
 
+# Flatten the args dictionary into a list of command line arguments
+args_list = []
+for key, value in args.items():
+    if isinstance(value, list):
+        args_list.extend([f"--{key}"] + [str(v) for v in value])
+    else:
+        args_list.append(f"--{key}={value}")
+
+# Run the executable multiple times
 for run in range(num_runs):
     try:
         print(f"Run number {run+1}/{num_runs}")
